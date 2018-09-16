6df7ed8f7b8bbbb523bf10863a1857ba3346fb76
Liga Daine <liga.daine@daine-it.com>
2018-09-16 23:33:11 +0200

    Added styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'; 
+import './App.css'
 
 class App extends Component {
     render(){
         return (
-            <div>Countdown Chanm, App Component</div>
+            <div className="App">
+                <div className="App-title">Countdown to December 25, 2017</div>
+                <div>
+                    <div className="Clock-days">14 Days</div>
+                    <div className="Clock-hours">30 hours</div>
+                    <div className="Clock-minutes">15 minutes</div>
+                    <div className="Clock-seconds">20 seconds</div>
+                </div>
+                <input type="text" placeholder="New Date"/>
+                <button>Submit</button>
+            </div>
         )
     }
 }
